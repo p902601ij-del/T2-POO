db99403dd90114e45fc34d2c8e0e71d10c0d0e83
p902601ij-del <p902601ij@gmail.com>
2026-03-04 20:53:35 -0600

    cuarto ejercicio

---
+++ Tarea_POO/src/main/java/com/example/tarea_poo/Figuras.kt
@@ -0,0 +1,28 @@
+package com.example.tarea_poo
+
+import kotlin.math.PI
+
+interface Figura {
+    fun calcularArea(): Double
+    fun calcularPerimetro(): Double
+}
+class Rectangulo(val base: Double, val altura: Double) : Figura {
+    override fun calcularArea() = base * altura
+    override fun calcularPerimetro(): Double {
+        return  2 * (base + altura)
+    }
+}
+class Circulo(val radio: Double) : Figura {
+    override fun calcularArea(): Double {
+        return  PI * radio * radio
+    }
+    override fun calcularPerimetro(): Double {
+        return  2 * PI * radio
+    }
+}
+fun main() {
+    val rect = Rectangulo(5.0, 3.0)
+    val circ = Circulo(4.0)
+    println("Rectángulo - Área: %.2f, Perímetro: %.2f".format(rect.calcularArea(), rect.calcularPerimetro()))
+    println("Círculo - Área: %.2f, Perímetro: %.2f".format(circ.calcularArea(), circ.calcularPerimetro()))
+}
